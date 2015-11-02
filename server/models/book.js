27b05a1663f5eade34413a5a826717b69c7bd488
Max <frederick.schaefer@gmail.com>
2015-11-02 14:30:17 -0800

    mongoLab connect details try 2

--- server/models/book.js
+++ server/models/book.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 	autoIncrement = require('mongoose-auto-increment');
 
-var connection = mongoose.createConnection("mongodb://localhost/BookSchema");
+var connection = mongoose.createConnection("mongodb://maxschae4:5fddq3at@ds049104.mongolab.com:49104/dist-lib");
 
 autoIncrement.initialize(connection);
 
